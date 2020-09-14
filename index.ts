00f42a6940af7930a7e6f7b4280ecb3e1f68a549
andres <telescopio322@gmail.com>
2020-09-14 13:42:08 -0500

    st 4

--- index.ts
+++ index.ts
@@ -5,12 +5,12 @@ import router from "./routes/router";
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const server = new Server();
+const server = Server.instance;
 
 server.app.use( bodyParser.urlencoded({ extended: true }) );
 server.app.use( bodyParser.json() );
 
-//CORS
+//CORS          
 server.app.use( cors({ origin: true, credentials: true }) );
 
 server.app.use('/', router);
